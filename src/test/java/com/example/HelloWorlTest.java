e221897063f48298903fcb4f292d05409e62867b
liujiangang <liujiangang@kuaishou.com>
2020-02-24 21:28:51 +0800

    bugfix

--- src/test/java/com/example/HelloWorlTest.java
+++ src/test/java/com/example/HelloWorlTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HelloWorlTest {
 	@Test
 	public void testHelloWorld() {
-		assertEquals("HelloWorld", "HelloWorl");
+		assertEquals("HelloWorld", "HelloWorld");
 	}
 
 }
