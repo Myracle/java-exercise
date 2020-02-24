344d88c4c3f649199d5b9ad01c5dcb6aaf3bf9da
liujiangang <liujiangang@kuaishou.com>
2020-02-24 21:13:23 +0800

    init

---
+++ src/test/java/com/example/HelloWorlTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorlTest {
+	@Test
+	public void testHelloWorld() {
+		assertEquals("HelloWorl", "HelloWorl");
+	}
+
+}
